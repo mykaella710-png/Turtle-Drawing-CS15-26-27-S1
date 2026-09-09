50daeacad74a4de4fd94c972275d73c74ae02717
mykaella710-png <mykaella710@gmail.com>
2026-09-09 14:53:40 -0600

    Turtle Drawing Assignment
    Trying turtle

---
+++ Root/main.py
@@ -0,0 +1,82 @@
+from turtle import*
+color("red")
+left(90)
+forward(100)
+color("blue")
+right(90)
+forward(100)
+color("green")
+left(90)
+backward(100)
+color("yellow")
+right(90)
+backward(100)
+
+reset()
+
+letter_size = 30
+gap = 10
+
+penup()
+# Account for 'H' and 'e' and the gaps between them
+backward_length = (letter_size * 2) + (gap * 2)
+
+backward(backward_length)
+
+# H
+pendown()
+left(90)
+forward(letter_size * 2)
+backward(letter_size)
+right(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+backward(letter_size * 2)
+right(90)
+penup()
+forward(gap)
+
+# e
+pendown()
+left(90)
+forward(letter_size)
+right(90)
+forward(letter_size)
+right(90)
+forward(letter_size / 2)
+right(90)
+forward(letter_size)
+left(90)
+forward(letter_size / 2)
+left(90)
+forward(letter_size)
+penup()
+forward(gap)
+
+# l
+pendown()
+left(90)
+forward(letter_size * 2)
+right(90)
+penup()
+forward(gap)
+
+# l
+pendown()
+right(90)
+forward(letter_size * 2)
+left(90)
+penup()
+forward(gap)
+
+# o
+pendown()
+forward(letter_size)
+left(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+left(90)
+forward(letter_size)
+done()
